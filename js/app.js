1d77e6cbe058ef96fb762231b2732cb345a77a00
ZiyouddinMominov <muminovz2009@gmail.com>
2023-12-11 05:35:36 +0500

    salom

---
+++ js/app.js
@@ -0,0 +1,71 @@
+// UYGA VAZIFA 1 - TAYYOR
+// function numbers(arr) {
+//   let sum = 0;
+//   for (const item of arr) {
+//     if (item % 3 == 0 || item % 5 == 0) {
+//       sum += item ** 2;
+//     }
+//   }
+//   return sum;
+// }
+// console.log(numbers([1, 2, 3, 4, 5]));
+// UYGA VAZIFA 2 I DON'T KNOW
+// UYGA VAZIFA 3 - TAYYOR
+// function sum1(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 == 1) {
+//       sum += arr[i];
+//     }
+//   }
+//   return sum;
+// }
+// console.log(sum1([1, 2, 3, 4, 5, 6, 7]));
+// 4-misol
+
+// function ex4(arg ){
+//     let sum = [ ];
+// for (const iterator of arg) {
+//         if (iterator % 2 == 1) {
+//             sum++;
+//         }
+//     }
+//     return sum
+// }
+
+// console.log(ex4([1, 2, 3, 4, 5]));
+
+// 6-misol
+
+// function ex6(arg){
+//     let sum = 0;
+//     let res = 0;
+//     for (const iterator of arg) {
+//         for (let i = 1; i <= iterator; i++){
+//             if (iterator % i == 0) {
+//                 sum++;
+//             }
+//             if (sum == 2){
+//                 res++;
+//             }
+//         }
+
+//     }
+//     return res
+// }
+
+// console.log(ex6([21, 42, 13, 14, 15, 16, 17]));
+
+// 7-misol
+
+// function ex7(arg ){
+//     let sum = 1;
+// for (const iterator of arg) {
+//         if (iterator % 7 == 0 && iterator % 3 == 0) {
+//             sum *= iterator;
+//         }
+//     }
+//     return sum
+// }
+
+// console.log(ex7([21, 42, 13, 14, 15, 16, 17]));
